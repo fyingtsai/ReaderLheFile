37179cf314c5cc26e3a1101f7d5693b06b45553d
Fang-Ying Tsai <fyingtsai@gmail.com>
2015-03-23 22:24:59 +0800

    Add to get the value of tan(beta)

--- lheReader.py
+++ lheReader.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python2.7
 # Author: Kelly Tsai
 
 import sys
@@ -9,11 +8,14 @@ class GetValue():
     def __init__(self):
     	self.zpMass = []
     	self.weight = []
-    	self.tanBeta = []
+    	self.mh_2 = []
+    	self.mh = []
 
 lheWeightList = []
 lheZpMassList = []
-Str = 'Integrated weight (pb)'
+str_weight = 'Integrated weight (pb)'
+str_Mh = 'Mh'
+str_mh__2 = 'mh__2'
 path = '/Users/KellyTsai/Documents/MadGraph/Zp_hA0_bb_hleFile/*.lhe'   
 files = glob.glob(path) 
 
@@ -24,16 +26,29 @@ def store_ZpMass(fileName):
 		s.zpMass = int(mass)
 		return s
 
+def getMhValue(readLine):
+	mhValue = float(readLine.split(' # ')[0].split()[1])
+	return mhValue
+
+def getmh_2Value(readLine):
+	mh__2Value = float(readLine.split(' # ')[0].split()[1])
+	return mh__2Value
+
 def getLheList():
 	for name in files: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
 		s = store_ZpMass(name)
 	    	try:
 	    	    with open(name) as f: # No need to specify 'r': this is the default.
 	    	    	for lheFile in f:
-						if lheFile.find(Str) > 0:
-							weightValue = lheFile.split(' : ')[1]
-							s.weight = weightValue
-							lheWeightList.append(s)
+	    	    		if lheFile.find(str_mh__2) > 0:
+	    	    			s.mh_2 = getmh_2Value(lheFile)
+	    	    		elif lheFile.find(str_Mh) > 0:
+	    	    			s.mh = getMhValue(lheFile)
+	    	    		# s.tanBeta = int(mh)/int(mh_2)
+	    	    		elif lheFile.find(str_weight) > 0:
+						weightValue = lheFile.split(' : ')[1]
+						s.weight = weightValue
+						lheWeightList.append(s)
 	    	except IOError as exc:
 	    	    if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
 	    	        raise # Propagate other kinds of IOError.
@@ -42,8 +57,7 @@ def getLheList():
 
 def main():
 	List = getLheList()
-	print 'ZpMass:',List[0].zpMass," Xsec:",List[0].weight
-	# print a[2].weight
+	print List[2].mh_2/List[2].mh
 
 if __name__ == "__main__":
    main()
