b5735bff9de3388d07621e70222423bda9530b52
Fang-Ying Tsai <fyingtsai@gmail.com>
2015-03-25 15:10:20 +0800

    Add get_tan(beta)

--- lheReader.py
+++ lheReader.py
@@ -10,12 +10,14 @@ class GetValue():
     	self.weight = []
     	self.mh_2 = []
     	self.mh = []
+    	self.tanbeta = []
 
 lheWeightList = []
 lheZpMassList = []
 str_weight = 'Integrated weight (pb)'
 str_Mh = 'Mh'
 str_mh__2 = 'mh__2'
+str_tanBeta = '# Tb'
 path = '/Users/KellyTsai/Documents/MadGraph/Zp_hA0_bb_hleFile/*.lhe'   
 files = glob.glob(path) 
 
@@ -36,7 +38,11 @@ def getmh_2Value(readLine):
 
 def getWeightValue(readLine):
 	weightValue = float(readLine.split(' : ')[1])
-	return weightValue	 
+	return weightValue	
+
+def getTanBeta(readLine):
+	tbValue  = float(readLine.split(' # ')[0].split()[1])
+	return tbValue 
 
 def getLheList():
 	for name in files: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
@@ -50,6 +56,8 @@ def getLheList():
 	    	    			s.mh = getMhValue(lheFile)
 	    	    		elif lheFile.find(str_weight) > 0:
 	    	    			s.weight = getWeightValue(lheFile)
+	    	    		elif lheFile.find(str_tanBeta) > 0:
+	    	    			s.tanbeta = getTanBeta(lheFile)
 					lheWeightList.append(s)
 	    	except IOError as exc:
 	    	    if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
@@ -67,7 +75,7 @@ def main():
 	for m in List:
 		xMass.append(m.zpMass)
 		yWeight.append(m.weight)
-		yTanBeta.append(m.mh_2/m.mh)
+		yTanBeta.append(m.tanbeta)
 	x = array("d", xMass)
 	y = array("d", yWeight)
 	y1 = array("d",yTanBeta)
@@ -79,13 +87,14 @@ def main():
 	g.SetMarkerStyle(21)
 	g.Write('AP')
 	g2.GetXaxis().SetTitle("ZpMass (GeV)")
-	g2.GetYaxis().SetTitle("tan(beta) (mh_2/mh)")
+	g2.GetYaxis().SetTitle("tan(beta)")
 	g2.SetMarkerColor(4)
 	g2.SetMarkerStyle(21)
 	g2.Write('AP')
 	# f.Write()
 	f.Close()
 	c1.Update()
-	# print List[2].mh_2/List[2].mh
+	print 'Mass:',xMass
+	print 'Weight: ',yWeight
 if __name__ == "__main__":
    main()
